88f958e7bf6e03092b976628fb82dd605cd78296
Martin Tovara <martintovara.acr@gmail.com>
2024-10-02 13:21:22 +0200

    Final improvements

--- js/conf/gallery.js
+++ js/conf/gallery.js
@@ -1,1 +1,1 @@
-const galleryImgs = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.jpg'];
+const galleryImgs = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg'];
--- js/conf/supportingUs.js
+++ js/conf/supportingUs.js
@@ -4,14 +4,4 @@ const dataSupportingUs = [
         name: 'Lorem ipsum company',
         web: 'www.example.com',
     },
-    {
-        logo: 'logo2.png',
-        name: 'Lorem ipsum company 2',
-        web: 'www.example2.com',
-    },
-    {
-        logo: 'logo3.png',
-        name: 'Lorem ipsum company 3',
-        web: 'www.example3.com',
-    },
 ];
