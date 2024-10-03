e848647efc1f2628edc6d116939c7421b3dd0bb3
Martin Tovara <martintovara.acr@gmail.com>
2024-10-03 10:43:59 +0200

    Moved observer from onDOMContentLoaded

--- js/init.js
+++ js/init.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const observer = new MutationObserver((_mutations, observerIn) => {
-        const main = document.getElementsByClassName('banner-div')[0];
-        if (main) {
-            setMarginMain();
-            observerIn.disconnect();
-        }
-    });
-    observer.observe(document.body, { childList: true, subtree: true });
+const observer = new MutationObserver((_mutations, observerIn) => {
+    const main = document.getElementsByClassName('banner-div')[0];
+    if (main) {
+        setMarginMain();
+        observerIn.disconnect();
+    }
+});
+observer.observe(document.body, { childList: true, subtree: true });
 
+document.addEventListener('DOMContentLoaded', () => {
     addCarouselGalleryImgs();
     themeBasedOnUser();
 });
